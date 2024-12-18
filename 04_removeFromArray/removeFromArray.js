5c5b87d70778a9d31ec9ffc14fc3220fa9bb2d8e
Sanar <san-ar@users.noreply.github.com>
2024-12-18 22:13:38 +0800

    Add solution for exercise 1-3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(text) {
+    let startIndex;
+    let endIndex;
+    let newArray;
+    
+    for (let i = 0; i < text.length; i++){
+        if (text[i] == "[") {
+            startIndex = i;
+        }
+        if (text[i] == "]") {
+            endIndex = i;
+        }
+    }
+
+    for (let i = 0; i < endIndex; i++){
+        newArray = newArray.push(text[i]);
+    }
+    // let requiredString = text.slice(startIndex + 1, endIndex);
+    
+    // let trimmedString = requiredString.replaceAll(/\s+/g, "");
+    // console.log(trimmedString);
+    
+    // stringArray = trimmedString.split(",");
+    // console.log(stringArray);
+    
+    // let numberArray = stringArray.map(Number);
+
+    // for ()
+    return newArray;
 };
 
 // Do not edit below this line
