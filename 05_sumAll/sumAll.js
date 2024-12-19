49f7ccb99667eaf87ba581ebed92abb857454e02
Sanar <san-ar@users.noreply.github.com>
2024-12-19 19:51:20 +0800

    Add solution for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (lowerLimit, upperLimit) {
 
+    let sum = 0;
+
+    if (lowerLimit < 0 || !Number.isInteger(lowerLimit) || upperLimit < 0 || !Number.isInteger(upperLimit)) {
+        return "ERROR"
+    }
+
+    if (lowerLimit > upperLimit) {
+        for (let i = upperLimit; i <= lowerLimit; i++) {
+            sum += i;
+
+        }
+    } else {
+        for (let i = lowerLimit; i <= upperLimit; i++) {
+            sum += i;
+
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
