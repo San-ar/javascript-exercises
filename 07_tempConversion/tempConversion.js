85d9c1c7646e55dce0dcfc846035bb9daa86f918
Sanar <san-ar@users.noreply.github.com>
2024-12-19 20:37:28 +0800

    Add Solution for exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsiusTemp = (fahr - 32) * 5 / 9;
+  return +celsiusTemp.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahrenTemp = (cel * 9/5) + 32;
+  return +fahrenTemp.toFixed(1);
 };
 
 // Do not edit below this line
