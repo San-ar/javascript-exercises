492b9fb1c752e7c8e8e91db3bf027da09ec630ea
Sanar <san-ar@users.noreply.github.com>
2025-01-03 17:55:18 +0800

    Add function to retuen a specific member of the Fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (limit) {
+    const arr = [];
+    let n = 0;
 
+    if (limit == 0) {
+        arr.push(0);
+        return arr[0];
+    } else if (limit < 0) {
+        return "OOPS";
+    } else {
+        for (let i = 0; i < limit; i++) {
+            if (arr.length < 2) {
+                arr.push(1);
+            } else {
+                let val = arr[n] + arr[n + 1];
+                arr.push(val);
+                n++;
+            }
+        }
+        return arr[arr.length - 1];
+    }
 };
 
 // Do not edit below this line
