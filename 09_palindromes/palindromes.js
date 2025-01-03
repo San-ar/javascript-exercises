f07d452603b48156f8dfdd7de875ca61a4c64f5a
Sanar <san-ar@users.noreply.github.com>
2025-01-03 17:09:23 +0800

    Add function for determining palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+function isLetter(char){
+    return  /[a-zA-Z0-9 ]/.test(char);
+}
 
+const palindromes = function (str) {
+    const wholeArr = str.replace(/\s/g, "").split("");
+    const filteredArr = wholeArr.filter(isLetter).map((item) => item.toUpperCase());
+    const reversedArr = wholeArr.filter(isLetter).reverse().map((item) => item.toUpperCase());
+
+    return JSON.stringify(filteredArr) === JSON.stringify(reversedArr);
 };
 
 // Do not edit below this line
