8f9eb1a7379bfb257a7c02b286bef7270a2f468c
Sanar <san-ar@users.noreply.github.com>
2025-01-03 18:09:45 +0800

    Add function for retrieving book titles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const bookArr = arr.map((obj) => obj.title);
+    return bookArr;
 };
 
 // Do not edit below this line
